e77d7d0d61595ccdf631277a023c849ec272b321
fergmac <f.macconnell@gmail.com>
2017-03-21 14:29:27 -0700

    build Gallery component

---
+++ src/Gallery.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class Gallery extends Component {
+    render() {
+        console.log('gallery props', this.props);
+        const { tracks } = this.props; 
+        return (
+            <div>
+            {tracks.map((track, index) => {
+                const trackImg = track.album.images[0].url;
+                return (
+                    <div key={index} className="track">
+                        <img src={trackImg} className="track-image" alt="track"/>
+                        <p className="track-text">{track.name}</p>
+                    </div>
+                )
+                })
+            }
+            </div>
+        )
+    }
+}
+
+export default Gallery;
