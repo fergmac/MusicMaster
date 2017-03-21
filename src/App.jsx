ed2facdaa15f79ba561ec7aaac66d119634e6617
fergmac <f.macconnell@gmail.com>
2017-03-21 14:30:52 -0700

    create Get request to api and import  Gallery and Profile components into App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,45 @@ import React, { Component } from 'react';
 
 import './App.css';
 import {FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
+import Profile from './Profile';
+import Gallery from './Gallery';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state= {
+            query: '',
+            artist: null,
+            tracks: []
+        }
+    }
+    search() {
+        console.log('this.state', this.state);
+        const BASE_URL = 'https://api.spotify.com/v1/search?';
+        let FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+        const ALBUM_URL = 'https://api.spotify.com/v1/artists/'
+        fetch(FETCH_URL, {
+            method: 'GET'
+        })
+        .then(response => response.json())
+        .then(json => {
+            const artist = json.artists.items[0];
+            console.log('artist', artist);
+            this.setState({artist});
+
+            FETCH_URL = `${ALBUM_URL}${artist.id}/top-tracks?country=US&`
+            fetch(FETCH_URL, {
+                method: 'GET'
+            })
+            .then(response => response.json())
+            .then(json => {
+                console.log('artist\'s top tracks', json);
+                //es6 destructured json.tracks
+                const {tracks} = json;
+                this.setState({tracks});
+            })
+        });
+    }
     render () {
         return (
             <div className="app">
@@ -13,19 +50,29 @@ class App extends React.Component {
                         <FormControl 
                         type="text"
                         placeholder="Search for an Artist"
+                        value={this.state.query}
+                        onChange={event => {this.setState({query: event.target.value})}}
+                        onKeyPress={event => {
+                            if (event.key === 'Enter') {
+                                this.search();
+                            }
+                        }}
                     />
-                    <InputGroup.Addon>
+                    <InputGroup.Addon onClick={() => this.search()}>
                         <Glyphicon glyph="search"></Glyphicon>
                     </InputGroup.Addon>
                     </InputGroup>
                 </FormGroup>
-                <div className="artist-profile">
-                    <div>Artist Name</div>
-                    <div>Artist Picture</div>
-                </div>
-                <div className="galler">
-                    Gallery
-                </div>
+                {
+                    this.state.artist !== null ?
+                    <div>
+                        <Profile 
+                            artist={this.state.artist}
+                        />
+                        <Gallery tracks={this.state.tracks}/>
+                    </div>
+                    : <div></div>
+                }
             </div>
         )
     }
